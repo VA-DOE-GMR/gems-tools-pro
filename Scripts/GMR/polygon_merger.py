273ecf5ca1dc649489cbec1aa63795add9a2c5e9
ajflink001 <147882783+ajflink001@users.noreply.github.com>
2024-04-25 09:36:57 -0400

    Add files via upload

--- Scripts/GMR/polygon_merger.py
+++ Scripts/GMR/polygon_merger.py
@@ -3,16 +3,44 @@ import gc
 from misc_arcpy_ops import default_env_parameters
 import sys
 from array import array
+from string import punctuation,digits,ascii_letters
 
 gdb_path = sys.argv[1]
 
-# Typos for MapUnits are not accounted due to too many variable scenarios to consider.
+double_puncts = tuple([punct * 2 for punct in array('u',list(punctuation))])
+alphanum = frozenset(list(f'{digits}{ascii_letters}'))
+
+# returning None indicates that is nothing of importance in that string.
+def fixFieldItemString(entry_string : str):
+
+    # No String entry should have consecutive spaces.
+    while entry_string.find('  ') != -1:
+        entry_string = entry_string.replace('  ',' ')
+    # No String entry should be begin and/or end with a space.
+    entry_string = entry_string.strip()
+    # No String entry should have two or more consecutive punctuation characters.
+    if entry_string == '':
+        return None
+    for double_punct in double_puncts:
+        if double_punct in entry_string:
+            while double_punct in entry_string:
+                entry_string = entry_string.replace(double_punct,double_punct[0])
+    for item in entry_string:
+        if item in alphanum:
+            return entry_string
+
+    return None
+
+
+# Typos for MapUnit field are not accounted due to too many variable scenarios to consider it practical.
 def mergeMatchingPolygons(gdb_path : str):
 
     '''
     This automatically merges polygons with the same MapUnit bordering each
-    other that also have matching relevant fields, excluding polygons with blank
-    or Null fields.
+    other that also have matching relevant fields, excluding Notes field.
+    Redundant spaces will be automatically corrected and explicit typos will be
+    automatically corrected as well (e.g., two or more consecutive
+    punctionations and/or nonalphanumeric characters).
     '''
 
     try:
@@ -25,107 +53,82 @@ def mergeMatchingPolygons(gdb_path : str):
 
     default_env_parameters()
 
-    required_fields = ('MapUnit','IdentityConfidence','Label','Symbol','DataSourceID','Notes')
-
     arcpy.AddMessage("Iterating through all polygons present in geodatabase...")
 
     for dataset in tuple(arcpy.ListDatasets()):
         for fc in tuple(arcpy.ListFeatureClasses(feature_dataset=dataset,feature_type='Polygon')):
+            arcpy.AddMessage(f"Merging polygons in {fc}...")
+            required_fields = ['MapUnit','IdentityConfidence','Label','Symbol','DataSourceID','Notes']
             fields = tuple([field.name for field in tuple(arcpy.ListFields(f'{dataset}/{fc}'))])
-            # This is to ensure that the OID/OBJECTID field name is correct as it has not been consistent at times.
             oid_name = fields[0]
-            for required in required_fields:
-                if not required in fields:
+            for required_field in required_fields:
+                if not required_field in fields:
                     oid_name = None
                     break
             if oid_name is None:
-                # Skip feature class because it is missing essential fields.
                 continue
-            fields = list(required_fields)
-            fields.insert(0,oid_name)
-            ignore = set()
+            required_fields.insert(0,oid_name)
+            max_notes_length = 0
+            for field in tuple(arcpy.ListFields(f'{dataset}/{fc}',field_type='String')):
+                if field.name == 'Notes':
+                    max_notes_length = field.length
+                    break
             full_iteration = False
+            ignore = set()
             while not full_iteration:
-                # If this never changes to false, it means that no more polygons need to be merged.
                 full_iteration = True
                 arcpy.management.MakeFeatureLayer(f'{dataset}/{fc}','temp_lyr')
-                for row in arcpy.da.SearchCursor(f'{dataset}/{fc}',fields):
+                for row in arcpy.da.SearchCursor('temp_lyr',required_fields):
                     if row[0] in ignore:
                         continue
-                    elif row[1] is None:
-                        # Ignore polygon with no MapUnit value.
+                    elif row[1] in (None,''):
                         ignore.add(row[0])
                         continue
-                    elif row[1].replace(' ','') == '':
-                        # Ignore polygon with blank MapUnit value.
-                        ignore.add(row[0])
-                        continue
-                    # Establish initial set of field values for polygon that will be created from merging.
-                    current_poly_info = [row[1],row[2],row[3],row[4],row[5],row[6]]
-
-                    # Select current polygon only.
+                    saved_info = [row[x] for x in range(7)]
+                    for n in range(2,len((saved_info := [row[x] for x in range(7)]))):
+                        if not saved_info[n] is None:
+                            saved_info[n] = fixFieldItemString(saved_info[n])
                     selected_polys = arcpy.management.SelectLayerByAttribute('temp_lyr','NEW_SELECTION',f'{oid_name} = {row[0]}')
-                    # Select current polygon and all bordering/adjacent polygons in the same feature class.
                     selected_polys = arcpy.management.SelectLayerByLocation(selected_polys,'BOUNDARY_TOUCHES','temp_lyr')
-
-                    # Keep information on polygons that have the same MapUnit value as indicated for current_poly_info (i.e. current polygon basing selection upon)
-                    valid_polys = {item[0] : (item[2],item[3],item[4],item[5],item[6]) for item in arcpy.da.SearchCursor(selected_polys,fields) if item[1] == current_poly_info[0]}
-
-                    # Remove MapUnit value as keeping it in memory is redundant.
-                    current_poly_info.pop(0)
-
-                    # Skip polygon if no there are no bordering/adjacent polygons that can be deduced to having the same MapUnit value.
-                    if len((oids := array('i',[valid_poly for valid_poly in tuple(valid_polys.keys())]))) > 1:
-                        for oid in oids:
-                            update_elements = False
-                            update_items = [None,None,None,None,None]
-                            deleted_key = False
+                    selected_info = {item[0] : (item[2],item[3],item[4],item[5],item[6]) for item in arcpy.da.SearchCursor(selected_polys,required_fields) if item[1] == saved_info[1]}
+                    if len((selected_keys := array('i',list(selected_info.keys())))) > 1:
+                        for selected_key in selected_keys:
                             for n in range(4):
-                                if not valid_polys[oid][n] is None:
-                                    if valid_polys[oid][n].replace(' ','') != '':
-                                        if current_poly_info[n] is None:
-                                            update_elements = True
-                                            update_items[n] = valid_polys[oid][n]
-                                        elif current_poly_info[n].replace(' ','') == '':
-                                            update_elements = True
-                                            update_items[n] = valid_polys[oid][n]
-                                        elif current_poly_info[n].replace(' ','') != valid_polys[oid][n].replace(' ',''):
-                                            valid_polys.pop(n)
-                                            deleted_key = True
+                                if saved_info[n+2] is None:
+                                    if not selected_info[selected_key][n] is None:
+                                        if not fixFieldItemString(selected_info[selected_key][n]) is None:
+                                            selected_info.pop(selected_key)
                                             break
-                            if update_elements and not deleted_key:
-                                for n in range(4):
-                                    if not update_items[n] is None:
-                                        current_poly_info[n] = update_items[n]
-                        # No matching values if condition is false, if any actual values were given.
-                        if len((oids := array('i',[valid_poly for valid_poly in tuple(valid_polys.keys())]))) > 1:
-                            # compiling Notes
+                                elif saved_info[n+2].replace(' ','') == '':
+                                    if not selected_info[selected_key][n] is None:
+                                        if not fixFieldItemString(selected_info[selected_key][n]) is None:
+                                            selected_info.pop(selected_key)
+                                            break
+                                elif not selected_info[selected_key][n] is None:
+                                    if fixFieldItemString(saved_info[n+2]) != fixFieldItemString(selected_info[selected_key][n]):
+                                        selected_info.pop(selected_key)
+                                        break
+                        if len((selected_keys := array('i',list(selected_info.keys())))) > 1:
                             full_iteration = False
-                            notes = []
-                            if not current_poly_info[4] is None:
-                                while current_poly_info[4].find('  ') != -1:
-                                    current_poly_info[4] = current_poly_info[4].replace('  ',' ')
-                                current_poly_info[4] = current_poly_info[4].strip()
-                                if current_poly_info[4] != '':
-                                    notes.append(current_poly_info[4])
-                            for oid in oids:
-                                if not (note_item := valid_polys[oid][-1]) is None:
-                                    while note_item.find('  ') != -1:
-                                        note_item = note_item.replace('  ',' ')
-                                    note_item = note_item.strip()
-                                    if note_item != '':
-                                        notes.append(note_item)
-                            if len((notes := tuple(notes))):
-                                current_poly_info[4] = f'{notes[0]}'
-                                for x in range(1,len(notes)):
-                                    current_poly_info[4] = f'{current_poly_info} | {notes[x]}'
+                            notes_items = set()
+                            if not saved_info[6] is None:
+                                notes_items.add(saved_info[6])
+                            for selected_key in selected_keys:
+                                if not selected_info[selected_key][4] is None:
+                                    temp_str = fixFieldItemString(selected_info[selected_key][4])
+                                    if not (temp_str := fixFieldItemString(selected_info[selected_key][4])) is None:
+                                        notes_items.add(temp_str)
+                            if len((notes_items := tuple(sorted(notes_items,key=str)))):
+                                saved_info[6] = notes_items[0]
+                                for n in range(1,len(notes_items)):
+                                    saved_info[6] = f'{saved_info[6]} | {notes_items[n]}'
+                                if max_notes_length < len(saved_info[6]):
+                                    arcpy.management.AlterField(f'{dataset}/{fc}','Notes',field_length=len(saved_info[6]))
                             else:
-                                current_poly_info[4] = None
-                            for oid in oids:
-                                valid_polys[oid][-1]
-                            select_str = f'{oid_name} IN ({oids[0]}'
-                            for n in range(1,len(oids)):
-                                select_str = f'{select_str},{oids[n]}'
+                                saved_info[6] = None
+                            select_str = f'{oid_name} IN ({selected_keys[0]}'
+                            for n in range(1,len(selected_keys)):
+                                select_str = f'{select_str},{selected_keys[n]}'
                             select_str = f'{select_str})'
                             arr = arcpy.Array()
                             for geom in (geoms := tuple([r[0] for r in arcpy.da.SearchCursor(f'{dataset}/{fc}',('SHAPE@',oid_name),select_str)])):
@@ -138,19 +141,19 @@ def mergeMatchingPolygons(gdb_path : str):
                                     counter += 1
                                     if counter == 1:
                                         row[0] = diss_geom
-                                        row[1] = current_poly_info[0]
-                                        row[2] = current_poly_info[1]
-                                        row[3] = current_poly_info[2]
-                                        row[4] = current_poly_info[3]
-                                        row[5] = current_poly_info[4]
+                                        row[1] = saved_info[2]
+                                        row[2] = saved_info[3]
+                                        row[3] = saved_info[4]
+                                        row[4] = saved_info[5]
+                                        row[5] = saved_info[6]
                                         cursor.updateRow(row)
                                     else:
                                         cursor.deleteRow()
-                            break
                         else:
                             ignore.add(row[0])
                     else:
                         ignore.add(row[0])
+
     try:
         arcpy.env.workspace = current_workspace[:]
     except Exception:
