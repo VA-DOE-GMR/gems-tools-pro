dcc2664b4236b7f24de681946db6edd5c725fb51
ajflink001 <147882783+ajflink001@users.noreply.github.com>
2024-06-07 11:28:22 -0400

    Update autofiller.py
    overhaul

--- Scripts/GMR/autofiller.py
+++ Scripts/GMR/autofiller.py
@@ -1,7 +1,7 @@
 import arcpy
 import gc
 from misc_arcpy_ops import default_env_parameters
-from misc_ops import fixFieldItemString,rgb_to_fgdc,ref_info
+from misc_ops import fixFieldItemString,ref_info,to_tuple
 import sys
 from array import array
 from re import sub as re_sub
@@ -216,58 +216,8 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
 
     edit.end_session()
 
-    # Autofill and Fixing DMU Symbol and AreaRGBFill fields
-    if enable_process[0] == 'true':
-
-        edit = GeMS_Editor()
-
-        arcpy.AddMessage('Autopopulating Symbol field in DescriptionOfMapUnits...')
-
-        for field in tuple(arcpy.ListFields('DescriptionOfMapUnits')):
-            oid_name = field.name
-            break
-
-        # prevents needing to create a range object with each iteration of the for-loop.
-        range_3 = range(3)
-
-        with arcpy.da.UpdateCursor('DescriptionOfMapUnits',(oid_name,'AreaFillRGB','Symbol')) as cursor:
-            for row in cursor:
-                update_row = False
-                if not row[1] is None:
-                    # Remove redundant characters.
-                    current_rgb = re_sub(r'[^0-9,]+','',row[1])
-                    if current_rgb.count(',') == 3:
-                        rgb_vals = []
-                        rgb_vals.append(current_rgb[:current_rgb.find(',')])
-                        current_rgb = current_rgb[current_rgb.find(',')+1:]
-                        rgb_vals.append(current_rgb[:current_rgb.find(',')])
-                        rgb_vals.append(current_rgb[current_rgb.find(',')+1:])
-                        for n in range_3:
-                            rgb_vals[n].zfill(3)
-                        if row[1] != (new_str := f'{rgb_vals[0]},{rgb_vals[1]},{rgb_vals[2]}'):
-                            row[1] = new_str
-                            update_row = True
-                        if row[2] != (new_symbol := rgb_to_fgdc(rgb_vals)):
-                            row[2] = new_symbol
-                            update_row
-                        if update_row:
-                            cursor.updateRow(row)
-                        del rgb_vals ; del new_str ; del new_symbol
-                    del current_rgb
-
-        del range_3
-
-        arcpy.AddMessage("Changes successfully applied.\n\nSaving edits...")
-
-        edit.end_session()
-
-        arcpy.AddMessage("Edits saved!\n\n")
-
-        del update_row ; del oid_name
-        gc.collect()
-
     # Autofill Label and Symbol fields of polygon feature classes
-    if enable_process[1] == 'true':
+    if enable_process[0] == 'true':
 
         # Fillout Symbol and Label fields for polygon feature classes in
         # geodatabase using corresponding information from DescriptionOfMapUnits
@@ -308,7 +258,7 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
         arcpy.AddMessage("Edits saved!\n\n")
 
     # Autofill MapUnit fields in point feature classes
-    if enable_process[2] == 'true':
+    if enable_process[1] == 'true':
 
         arcpy.AddMessage("Filling out MapUnit field of point feature classes in geodatabase based upon location relative to polygons in MapUnitPolys...\n")
 
@@ -376,7 +326,7 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
         arcpy.AddMessage("Edits saved!\n\n")
 
     # Alphabetize Glossary and Add missing terms
-    if enable_process[3] == 'true':
+    if enable_process[2] == 'true':
 
         arcpy.AddMessage("Alphabetizing and adding missing terms to Glossary...")
 
@@ -417,7 +367,7 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
         with arcpy.da.UpdateCursor('Glossary',('Term','Definition','DefinitionSourceID')) as cursor:
             for row in cursor:
                 update_row = False
-                if None in (tester := set((row[0],row[1],row[2]))) and len(tester) == 1:
+                if None in (tester := set((row[0],row[1],row[2]))) and len(tester) == 1 and not row[2] is None:
                     del tester
                     continue
                 del tester
@@ -486,7 +436,7 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
         arcpy.AddMessage("Edits successfully saved!\n\n")
 
     # Autopopulate DataSources table
-    if enable_process[4] == 'true':
+    if enable_process[3] == 'true':
 
         arcpy.AddMessage('Filling out and populating DataSources table...')
 
@@ -630,6 +580,190 @@ def autofill_GeMS(gdb_path : str, enable_process : tuple):
         del naloe_zelmatitum
 
         gc.collect()
+
+
+    if enable_process[4] == 'true':
+
+        arcpy.AddMessage('Merging matching polygons...')
+
+        edit = GeMS_Editor()
+
+        # Explicit and constant range objects.
+        range_4 = range(4)
+        range_7 = range(7)
+
+        for dataset in tuple(arcpy.ListDatasets()):
+            for fc in tuple(arcpy.ListFeatureClasses(feature_dataset=dataset,feature_type='Polygon')):
+                arcpy.AddMessage(f"Merging polygons in {fc}...")
+                # Information gathered and required to create a merged version of
+                # polygons in a polygon feature class.
+                polygon_mergers = []
+                # This is to prevent this tool from failing if non-GeMS/FGDC feature
+                # classes are encountered that may not have all these fields.
+                required_fields = ['MapUnit','IdentityConfidence','Label','Symbol','DataSourceID','Notes']
+                feature_item = f'{dataset}/{fc}'
+                fields = tuple([field.name for field in tuple(arcpy.ListFields(feature_item))])
+                oid_name = fields[0]
+                # Fast means of getting OID field name.
+                for required_field in required_fields:
+                    if not required_field in fields:
+                        oid_name = None
+                        break
+                if oid_name is None:
+                    continue
+                required_fields.insert(0,oid_name)
+                # This keeps track of the maximum number of characters currently
+                # allowed in the Notes field and number of maximum characters
+                # required to concatenate Notes from polygon feature that shall be
+                # appended to each other.
+                max_notes_length = 0
+                new_length = 0
+                for field in tuple(arcpy.ListFields(feature_item,field_type='String')):
+                    if field.name == 'Notes':
+                        max_notes_length = field.length
+                        new_length = field.length
+                        break
+                # This keeps track of which features have already been checked and
+                # will be skipped if already present in set.
+                ignore = set()
+                # Create Feature Layer to prevent odd slowdown and memory behavior
+                # of SelectLayerByLocation and SelectLayerByAttribute
+                # Management tools.
+                arcpy.management.MakeFeatureLayer(feature_item,'temp_lyr')
+                for row in arcpy.da.SearchCursor('temp_lyr',required_fields):
+                    if row[0] in ignore:
+                        continue
+                    saved_info = [row[x] for x in range_7]
+                    for n in range(2,len((saved_info := [row[x] for x in range_7]))):
+                        if not saved_info[n] is None:
+                            saved_info[n] = fixFieldItemString(saved_info[n])
+                    if saved_info[1] is None:
+                        continue
+                    selected_polys = arcpy.management.SelectLayerByLocation(arcpy.management.SelectLayerByAttribute('temp_lyr','NEW_SELECTION',f'{oid_name} = {row[0]}'),'BOUNDARY_TOUCHES','temp_lyr')
+                    selected_info = {item[0] : (item[2],item[3],item[4],item[5],item[6]) for item in arcpy.da.SearchCursor(selected_polys,required_fields) if item[1] == saved_info[1]}
+                    if len((selected_keys := array('i',selected_info.keys()))) > 1:
+                        # Matching MapUnit was found in touching polygons.
+                        for selected_key in selected_keys:
+                            for n in range_4:
+                                if saved_info[n+2] is None:
+                                    if not selected_info[selected_key][n] is None:
+                                        if not fixFieldItemString(selected_info[selected_key][n]) is None:
+                                            selected_info.pop(selected_key)
+                                            break
+                                elif saved_info[n+2].replace(' ','') == '':
+                                    if not selected_info[selected_key][n] is None:
+                                        if not fixFieldItemString(selected_info[selected_key][n]) is None:
+                                            selected_info.pop(selected_key)
+                                            break
+                                elif not selected_info[selected_key][n] is None:
+                                    if saved_info[n+2] != fixFieldItemString(selected_info[selected_key][n]):
+                                        selected_info.pop(selected_key)
+                                        break
+                        if len((selected_keys := array('i',selected_info.keys()))) > 1:
+                            # Identical information found in touching polygons.
+                            checked = {row[0]}
+                            sub_iteration_complete = False
+                            # This iterates through all matching touching polygons
+                            # and checks if they also touch matching polygons
+                            # that can be grouped together.
+                            while not sub_iteration_complete:
+                                sub_iteration_complete = True
+                                for selected_key in selected_keys:
+                                    if selected_key in checked:
+                                        continue
+                                    selected_polys = arcpy.management.SelectLayerByAttribute('temp_lyr','NEW_SELECTION',f'{oid_name} = {selected_key}')
+                                    selected_polys = arcpy.management.SelectLayerByLocation(selected_polys,'BOUNDARY_TOUCHES','temp_lyr')
+                                    checked.add(selected_key)
+                                    if len((new_info := {item[0] : (item[2],item[3],item[4],item[5],item[6]) for item in arcpy.da.SearchCursor(selected_polys,required_fields) if item[1] == saved_info[1] and not item[0] in selected_keys and not item[0] in checked})):
+                                        # Notes is the only field that does not need to be identical.
+                                        # IdentityConfidence, Label, Symbol, and DataSourceID all must match.
+                                        for new_key in array('i',new_info.keys()):
+                                            for n in range_4:
+                                                if saved_info[n+2] is None:
+                                                    if not new_info[new_key][n] is None:
+                                                        if not fixFieldItemString(new_info[new_key][n]) is None:
+                                                            new_info.pop(new_key)
+                                                            break
+                                                elif saved_info[n+2].replace(' ','') == '':
+                                                    if not new_info[new_key][n] is None:
+                                                        if not fixFieldItemString(new_info[new_key][n]) is None:
+                                                            new_info.pop(new_key)
+                                                            break
+                                                elif not new_info[new_key][n] is None:
+                                                    if saved_info[n+2] != fixFieldItemString(new_info[new_key][n]):
+                                                        new_info.pop(new_key)
+                                                        break
+                                        if len(new_info):
+                                            sub_iteration_complete = False
+                                            for new_key in array('i',new_info.keys()):
+                                                selected_info[new_key] = new_info[new_key]
+                                    del selected_polys
+                                if not sub_iteration_complete:
+                                    selected_keys = array('i',selected_info.keys())
+                            del checked ; del sub_iteration_complete
+                            gc.collect()
+                            # Create a concatenated string of differing Notes or
+                            # value to None if they are all blank.
+                            notes_items = set()
+                            if not saved_info[6] is None:
+                                notes_items.add(saved_info[6])
+                            for selected_key in selected_keys:
+                                if not selected_info[selected_key][4] is None:
+                                    temp_str = fixFieldItemString(selected_info[selected_key][4])
+                                    if not (temp_str := fixFieldItemString(selected_info[selected_key][4])) is None:
+                                        notes_items.add(temp_str)
+                            if len((notes_items := tuple(sorted(notes_items,key=str)))):
+                                saved_info[6] = notes_items[0]
+                                for n in range(1,len(notes_items)):
+                                    saved_info[6] = f'{saved_info[6]} | {notes_items[n]}'
+                                if new_length < len(saved_info[6]):
+                                    new_length = len(saved_info[6])
+                            else:
+                                saved_info[6] = None
+                            select_str = f'{oid_name} IN ({selected_keys[0]}'
+                            for n in range(1,len(selected_keys)):
+                                select_str = f'{select_str},{selected_keys[n]}'
+                            select_str = f'{select_str})'
+                            arr = arcpy.Array()
+                            for geom in (geoms := tuple([r[0] for r in arcpy.da.SearchCursor(feature_item,('SHAPE@',oid_name),select_str)])):
+                                for part in geom:
+                                    arr.add(part)
+                            polygon_mergers.append((saved_info,select_str,arcpy.Polygon(arr,geoms[0].spatialReference)))
+                            for selected_key in selected_keys:
+                                ignore.add(selected_key)
+                        else:
+                            # No matching information found in touching polygons.
+                            ignore.add(row[0])
+                        del selected_keys
+                    else:
+                        # No matching MapUnit value was found in touching polygons.
+                        ignore.add(row[0])
+                if max_notes_length < new_length:
+                    # Increase the maximum number of characters allowed for Notes.
+                    edit.end_session()
+                    arcpy.management.AlterField(feature_item,'Notes',field_length=new_length)
+                    edit = GeMS_Editor()
+                for n in range(len((polygon_mergers := tuple(polygon_mergers)))):
+                    counter = 0
+                    with arcpy.da.UpdateCursor(feature_item,('SHAPE@','IdentityConfidence','Label','Symbol','DataSourceID','Notes'),polygon_mergers[n][1]) as cursor:
+                        for row in cursor:
+                            counter += 1
+                            if counter == 1:
+                                # Overwrite one of the matching polygons shape and
+                                # Attribute Table values.
+                                row[0] = polygon_mergers[n][2]
+                                row[1] = polygon_mergers[n][0][2]
+                                row[2] = polygon_mergers[n][0][3]
+                                row[3] = polygon_mergers[n][0][4]
+                                row[4] = polygon_mergers[n][0][5]
+                                row[5] = polygon_mergers[n][0][6]
+                                cursor.updateRow(row)
+                            else:
+                                # Delete the rest of the matching polygons.
+                                cursor.deleteRow()
+                del oid_name ; del new_length ; del max_notes_length ; del fields ; del required_fields ; del feature_item ; del ignore
+                gc.collect()
+        edit.end_session()
 
     # Autofill _ID fields
     if enable_process[5] == 'true':
