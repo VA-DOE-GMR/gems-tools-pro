87ca6e0ce2838a42aef05b30e104b38209c16ef3
Jennie Latane <152326227+vlatane-va-energy@users.noreply.github.com>
2024-09-27 16:16:57 -0400

    Rename to GMR DataSources table
    Standardize terminology to 'GMR DataSources table', remove 'master'.

--- Scripts/GMR/check_DAS.py
+++ Scripts/GMR/check_DAS.py
@@ -6,7 +6,7 @@ import os
 
 def main(db_table):
     """
-    Check database DAS table against master DAS
+    Check database DAS table against GMR DataSources table
     """
 
     config_path = r"Z:\PROJECTS\MAPPING\GuidanceDocs\GeMS\gems-tools-pro-GMR"
@@ -18,7 +18,7 @@ def main(db_table):
         arcpy.env.workspace = os.path.join(config_path,'SDE_connection.sde')
         GMR_DAS = config['table']
         GMR_DAS = arcpy.MakeTableView_management(GMR_DAS, "view_of_table")
-        arcpy.AddMessage("Successfully connected to Master Data Sources table")
+        arcpy.AddMessage("Successfully connected to GMR DataSources table")
 
     except Exception:
         arcpy.AddMessage("Failed to access connection string and connect to SDE. See below error")
@@ -26,7 +26,7 @@ def main(db_table):
         arcpy.AddError(e.args[0])
         exit()
 
-    # turn Master DAS into df
+    # turn GMR DataSources table into df
     columns = [f.name for f in arcpy.ListFields(GMR_DAS)]
     df_GMR = pandas.DataFrame(data=arcpy.da.SearchCursor(GMR_DAS,columns),columns=columns)
     
@@ -35,12 +35,12 @@ def main(db_table):
     columns = [f.name for f in arcpy.ListFields(db_table)]
     df_gdb = pandas.DataFrame(data=arcpy.da.SearchCursor(db_table,columns),columns=columns)
 
-    # Find table records not identical to Master DAS
+    # Find table records not identical to GMR DataSources table
     df_merge = pandas.merge(df_gdb,df_GMR, on=['Source','DataSources_ID'],how='left',indicator='exists')
     df_merge['exists'] = df_merge['exists'].map(lambda x: x == 'both')
     df_filter = df_merge[df_merge['exists'] == False]
 
-    arcpy.AddMessage(f"Source(s) not matching the GMR Master DAS table:\n"
+    arcpy.AddMessage(f"Source(s) not matching the GMR DataSources table:\n"
                      f"{df_filter[['DataSources_ID','Source']]}\n"
                      f"Check these records and run again.")
 
