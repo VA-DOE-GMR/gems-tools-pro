0a8e687e0ee466321bd47891c9cb02648f6c349d
ajflink001 <147882783+ajflink001@users.noreply.github.com>
2025-12-12 13:33:02 -0500

    Update misc_ops.py
    Additional type hinting was added.

--- Scripts/misc_ops.py
+++ Scripts/misc_ops.py
@@ -10,7 +10,7 @@ class Referential_Information:
         self.idRootDict = {"CartographicLines": "CAL","ContactsAndFaults":"CAF","CMULines":"CMULIN","CMUMapUnitPolys":"CMUMUP","CMUPoints":"CMUPNT","CMUText":"CMUTXT","DataSources":"DAS","DataSourcePolys":"DSP","DescriptionOfMapUnits":"DMU","ExtendedAttributes":"EXA","FossilPoints":"FSP","GenericPoints":"GNP","GenericSamples":"GNS","GeochemPoints":"GCM","GeochronPoints":"GCR","GeologicEvents":"GEE","GeologicLines":"GEL","Glossary":"GLO","IsoValueLines":"IVL","MapUnitPoints":"MPT","MapUnitPolys":"MUP","MapUnitOverlayPolys":"MUO","MiscellaneousMapInformation":"MMI","OrientationPoints":"ORP","OtherLines":"OTL","OverlayPolys":"OVP","PhotoPoints":"PHP","RepurposedSymbols":"RPS","Stations":"STA","StandardLithology":"STL","MapUnitPointAnno24k":"ANO"}
         self.x_id_count = 0
 
-    def getRootName(self,fc_name) -> str:
+    def getRootName(self, fc_name : str) -> str:
         """
         This determines the prefix used for _ID ending fields in feature classes.
         """
@@ -31,7 +31,7 @@ class Referential_Information:
 ref_info = Referential_Information()
 
 
-def makeListIntArray(entry_list : list):
+def makeListIntArray(entry_list : list) -> array:
 
     try:
         return array('I',entry_list)
