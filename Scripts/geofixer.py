b55863a808687785f3eb88bd5a27100a1b8ddaf0
Adam J. Link <147882783+ajflink001@users.noreply.github.com>
2026-03-25 10:31:18 -0400

    Update geofixer.py
    Only datasets named GeologicMap or contain "CrossSection" in the name will explicitly be handled.

--- Scripts/geofixer.py
+++ Scripts/geofixer.py
@@ -60,7 +60,7 @@ def geofixer_GeMS(gdb_path : str, enable_process : tuple) -> None:
     # Selected features will disrupt how this tool functions. It will not cause
     # any errors or abnormal behavior; however, it will cause certain things to
     # be skipped or completely ignored by the tool.
-    deselectObjects((datasets := tuple(arcpy.ListDatasets())))
+    deselectObjects((datasets := tuple([item for item in arcpy.ListDatasets() if item == 'GeologicMap' or 'CrossSection' in item])))
 
     # For simplification purposes.
     class GeMS_Editor:
